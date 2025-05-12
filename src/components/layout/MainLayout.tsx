4f17f3db6f3df0a8a56b9becd4a10f762514e636
AliAshour2 <aliashour468@gmail.com>
2025-05-12 22:06:23 +0300

    enhance padding main elements

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -4,13 +4,12 @@ import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 
 const MainLayout = () => {
-  return (
-    <div className="flex min-h-screen flex-col">
+  return (    <div className="flex min-h-screen flex-col">
       <Navbar />
-      <main className="flex-grow">
+      <main className="flex-grow container mx-auto px-4 py-32">
         <Outlet />
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
